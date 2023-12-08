7b057f757f2372cfe3a4f5fc79379caa070eeb19
Howard-149 <b09901149@g.ntu.edu.tw>
2023-12-08 22:01:56 +0800

    pull

--- utils/auto.py
+++ utils/auto.py
@@ -1,9 +1,9 @@
 import os
 
-path = ['test3', 'test4']
+path = ['test_dots_and_boxes_3*3_apt_maxsim100']
 pcr_p = [0.5, 0.25]
 pcr_f = [0.5, 0.5]
 
-for i in range(2):
-    command = f"python runner.py --path=dab/{path[i]} --pcr=True --pcr_p={str(pcr_p[i])} --pcr_f={str(pcr_f[i])}"
+for i in range(len(path)):
+    command = f"python runner.py --path=/home/howard/RL/final_project/results/{path[i]} --apt=True --apt_w=0.15 "
     os.popen(command).read()
--- utils/battlefield.py
+++ utils/battlefield.py
@@ -7,55 +7,55 @@ import random
 # game = "tic_tac_toe"
 game = "dots_and_boxes"
 num_matches = 1
-num_games = 25
+num_games = 100
 # "/home/howard/RL/final_project/models/base"
-directory = "/home/howard/RL/final_project/results/test_dots_and_boxes_3*3_allFalse_1_maxsim100"
-mcts = [10,20,25,33,50,100,200,250,330,500,1000] # [2, 5, 10, 30, 100]
+directory = "/home/howard/RL/final_project/results/test_dots_and_boxes_3*3_apt_maxsim100"
+mcts = [1000] # [2, 5, 10, 30, 100]
 mcts2 = [10,20,25,33,50,100,200,250,330,500,1000]
-az = [i for i in range(0,1001,100)]
-log = "/home/howard/RL/final_project/logs/mcts.log"
+az = [i for i in range(100,101,100)]
+log = "/home/howard/RL/final_project/logs/test_dots_and_boxes_3*3_apt_maxsim100.log"
 
 FLAGS.game = game
 FLAGS.num_games = num_games
 FLAGS.log = log
+for m in range(num_matches):
+    for a in az:
+        for m in mcts:
+            FLAGS.az_path = directory + "/checkpoint-" + str(a)
+            FLAGS.az_path2 = directory + "/checkpoint-" + str(a)
+            FLAGS.max_simulations = m
+            FLAGS.max_simulations2 = m
+            FLAGS.player1 = "az"
+            FLAGS.player2 = "mcts"
+            try:
+                app.run(evaluation.main)
+            except Exception as error:
+                print("An error occurred:", error) 
+            FLAGS.player1 = "mcts"
+            FLAGS.player2 = "az"
+            try:
+                app.run(evaluation.main)
+            except Exception as error:
+                print("An error occurred:", error) 
 # for m in range(num_matches):
-#     for a in az:
-#         for m in mcts:
-#             FLAGS.az_path = directory + "/checkpoint-" + str(a)
-#             FLAGS.az_path2 = directory + "/checkpoint-" + str(a)
-#             FLAGS.max_simulations = m
-#             FLAGS.max_simulations2 = m
-#             FLAGS.player1 = "az"
+#     for m1 in mcts:
+#         for m2 in mcts2:
+#             # FLAGS.az_path = directory + "/checkpoint-" + str(a)
+#             # FLAGS.az_path2 = directory + "/checkpoint-" + str(a)
+#             FLAGS.max_simulations = m1
+#             FLAGS.max_simulations2 = m2
+#             FLAGS.player1 = "mcts"
 #             FLAGS.player2 = "mcts"
 #             try:
 #                 app.run(evaluation.main)
-#             except Exception as error:
-#                 print("An error occurred:", error) 
+#             except :
+#                 pass
 #             FLAGS.player1 = "mcts"
-#             FLAGS.player2 = "az"
+#             FLAGS.player2 = "mcts"
+#             FLAGS.max_simulations = m2
+#             FLAGS.max_simulations2 = m1
 #             try:
 #                 app.run(evaluation.main)
-#             except Exception as error:
-#                 print("An error occurred:", error) 
-for m in range(num_matches):
-    for m1 in mcts:
-        for m2 in mcts2:
-            # FLAGS.az_path = directory + "/checkpoint-" + str(a)
-            # FLAGS.az_path2 = directory + "/checkpoint-" + str(a)
-            FLAGS.max_simulations = m1
-            FLAGS.max_simulations2 = m2
-            FLAGS.player1 = "mcts"
-            FLAGS.player2 = "mcts"
-            try:
-                app.run(evaluation.main)
-            except :
-                pass
-            FLAGS.player1 = "mcts"
-            FLAGS.player2 = "mcts"
-            FLAGS.max_simulations = m2
-            FLAGS.max_simulations2 = m1
-            try:
-                app.run(evaluation.main)
-            except :
-                pass
+#             except :
+#                 pass
                
